d5fe583a0bf2a6f7a60d9f526dc72e2e375f7993
Gustaxz <guxtavogusteible@gmail.com>
2022-06-01 15:52:35 -0300

    funcionamento do railway

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(process.env.PORT ||3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('Servidor online!')
 })
 
