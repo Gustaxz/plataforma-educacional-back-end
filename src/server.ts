c2680a2ea2c0df3521c92e7228a0e6c5baaca06b
Gustaxz <guxtavogusteible@gmail.com>
2022-06-02 13:33:55 -0300

    consertando rotas

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.post('/', async (req, res) => {
     return res.send("Post enviado")
 })
 
-app.get('/', async (req, res) => {
+app.get('/subjects', async (req, res) => {
     const subject = req.query.subject as string
 
     const response = await prisma.post.findMany({
