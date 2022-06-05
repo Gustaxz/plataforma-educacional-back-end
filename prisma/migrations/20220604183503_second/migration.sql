b9ec748a524e07dab4d713b501dbec1c8fca9554
Gustaxz <guxtavogusteible@gmail.com>
2022-06-05 18:30:14 -0300

    sistema de login

---
+++ prisma/migrations/20220604183503_second/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "adm" BOOLEAN,
+ADD COLUMN     "teacher" BOOLEAN;
--- src/server.ts
+++ src/server.ts
@@ -52,3 +52,105 @@ app.get('/topics', async (req, res) => {
 
     res.send(JSON.stringify(response))
 })
+
+app.post('/users', async (req, res) => {
+
+    const {email, password, infos} = req.body
+
+    const response = await prisma.user.create({
+        data: {
+            email, 
+            password,
+            infos
+        }
+    }).then( (data) => res.send({error: false, data,})).catch((e) => res.send({error: true, data: e}))
+
+    return 
+
+})
+
+app.patch('/users/update', async (req, res) => {
+    const { id, data } = req.body
+
+    try {
+        const response = await prisma.user.update({
+            where: {
+                id,
+            },
+            data: {
+                infos: JSON.stringify(data)
+            }
+        }) 
+    return res.json({response})     
+    }
+    catch {
+        return res.json({error: '002', motive: 'User not found'})
+    }
+    
+})
+
+app.patch('/users/update/permissions', async (req, res) => {
+    const { id, adm, teacher } = req.body    
+
+    try {
+        const response = await prisma.user.update({
+            where: {
+                id,
+            },
+            data: {
+                adm,
+                teacher,
+            }
+        })
+        return res.json(response)
+    } catch  {
+        return res.json({error : '003', motive: "Something went wrong"})
+    }
+})
+
+app.get('/users', async (req, res) => {
+    const userId = req.query.user as string
+
+    try {
+        const response = await prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+
+        return res.json(response)
+    }
+
+    catch {
+        return res.json({error: '002', motive: 'User not found'})
+    }
+})
+
+app.get('/users/email', async (req, res) => {
+    const email = req.query.email as string
+
+    try {
+        const response = await prisma.user.findUnique({
+            where: {
+                email,
+            }
+        })
+
+        return res.json(response)
+    }
+
+    catch {
+        return res.json({error: '002', motive: 'User not found'})
+    }
+})
+
+app.get('/allusers', async (req, res) => {
+    try {
+        const response = await prisma.user.findMany()
+
+        return res.json(response)
+
+    } catch  {
+        return res.json({error : '003', motive: "Something went wrong"})
+    }
+})
